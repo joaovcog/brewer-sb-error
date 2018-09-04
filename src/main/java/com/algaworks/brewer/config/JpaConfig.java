7249339d28f1f9cc270c6249dd3d51563275f161
João Victor <joaovictor.cogomes@gmail.com>
2018-09-04 20:41:21 -0300

    Migrando para usar Spring Boot

--- src/main/java/com/algaworks/brewer/config/JpaConfig.java
+++ src/main/java/com/algaworks/brewer/config/JpaConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -24,15 +23,6 @@ import com.algaworks.brewer.repository.CervejaRepository;
 @EnableJpaRepositories(basePackageClasses = CervejaRepository.class, enableDefaultTransactions = false)
 @EnableTransactionManagement
 public class JpaConfig {
-
-	@Bean
-	public DataSource dataSource() {
-		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
-
-		dataSourceLookup.setResourceRef(true);
-
-		return dataSourceLookup.getDataSource("jdbc/brewerDB");
-	}
 
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter() {
--- src/main/java/com/algaworks/brewer/storage/FotoStorage.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorage.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface FotoStorage {
 	
 	public static final String THUMBNAIL_PREFIX = "thumbnail.";
+	public final String URL = "http://localhost:8080/fotos/";
 	
 	public String salvar(MultipartFile[] files);
 
